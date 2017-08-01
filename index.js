f6e66d1ada6e0d1984aca30f39b71419bda8c501
Anton Morozov <anton.morozov.16@ucl.ac.uk>
2017-08-01 23:24:32 +0100

    removing accidental /src files

--- index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
